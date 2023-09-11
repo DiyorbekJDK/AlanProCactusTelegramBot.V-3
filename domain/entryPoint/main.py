71d41050208e25967e0bcf39d55924e2c54cff69
Diyorbek-JDK <diyorbekdavronov07072007@gmail.com>
2023-09-11 20:50:25 +0500

    Some changes to run bot in local server

--- domain/entryPoint/main.py
+++ domain/entryPoint/main.py
@@ -1,5 +1,10 @@
-from domain.mainCode.all_code import allCode
+from pathlib import Path
+import sys
 
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+
+from domain.mainCode.all_code import allCode
 
 
 ##############################
